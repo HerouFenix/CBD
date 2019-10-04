7fe4331db0fd9f417389fad99406f6a45de17c37
DS <ds@pop-os.localdomain>
2019-10-04 16:05:34 +0100

    continued p1

--- Pratica1/Ex3/ExB.py
+++ Pratica1/Ex3/ExB.py
@@ -22,10 +22,10 @@ class SimplePostList:
         self.key = "users" #Key set for users' name
 
     def save_user_left(self,username):
-        self.r.lpush('users',username)
+        self.r.lpush(self.key,username) #Note: No clue why this is bugging out. Both self.key and username are of <class 'str'> so this should work...
     
     def save_user_right(self,username):
-        self.r.rpush('users',username)
+        self.r.rpush(self.key,username)
 
     def get_user(self):
         all_users = []
@@ -40,7 +40,7 @@ class SimplePostList:
 class SimplePostHash:
     def __init__(self):
         self.r = redis.Redis()
-        self.key = "users" #Key for users' name
+        self.key = "people:users:" #Key for users' name
         self.incrementer = -1
         self.name = "people" #Hash map's name
    
@@ -62,19 +62,15 @@ def main():
 
     #Set some users
     users = ["DS", "Claudia", "Vasco", "Vasconcelos"]
+    
+    print("##############Set##############")
     for username in users:
         post_set.save_user(username)
     
     print(post_set.get_all_keys())
     print(post_set.get_user())
 
-    post_list = SimplePostList()
-    for username in users:
-        post_list.save_user_left(username)
-
-    print(post_list.get_all_keys())
-    print(post_list.get_user())
-
+    print("##############Hash##############")
     post_hash = SimplePostHash()
     for username in users:
         post_hash.save_user(username)
@@ -82,7 +78,13 @@ def main():
     print(post_hash.get_all_keys()) 
     print(post_hash.get_user())
 
+    print("##############List##############")
+    post_list = SimplePostList()
+    for username in users:
+        post_list.save_user_left("oof")
 
+    print(post_list.get_all_keys())
+    print(post_list.get_user())
 
 if __name__ == '__main__':
     main()
